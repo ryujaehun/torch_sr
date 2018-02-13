9e1dd03a5c44e0c405575a622f4d6d2925495d2d
JaeJunRyu <2013103902@khu.ac.kr>
2018-02-13 14:14:50 +0900

    update

--- main1.py
+++ main1.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from mymodel1 import Net
+from mymodel3 import Net
 from data import get_training_set, get_test_set
 import datetime,random,os,csv
 # Training settings
@@ -18,7 +18,7 @@ parser.add_argument('--batchSize', type=int, default=64, help='training batch si
 parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch size')
 parser.add_argument('--nEpochs', type=int, default=2, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
-parser.add_argument('--cuda', action='store_true', required=True,help='use cuda?')
+parser.add_argument('--cuda', action='store_true' ,help='use cuda?')
 parser.add_argument('--threads', type=int, default=12, help='number of threads for data loader to use')
 #parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
 opt = parser.parse_args()
@@ -64,14 +64,14 @@ criterion = nn.MSELoss()
 
 if cuda:
     
-    if torch.cuda.device_count() > 1:
-        print("Let's use", torch.cuda.device_count(), "GPUs!")
-        model = nn.DataParallel(model)
+    #if torch.cuda.device_count() > 1:
+    #    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    #    model = nn.DataParallel(model)
        # criterion=nn.DataParallel(criterion)
 
-    if torch.cuda.is_available():
+    #if torch.cuda.is_available():
     
-        model = model.cuda()
+    model = model.cuda()
     criterion = criterion.cuda()
 
 optimizer = optim.Adam(model.parameters(), lr=opt.lr)
