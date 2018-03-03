f2e3d6c43beb4ee84223fe2b17c446b04acbf679
ryujaehun <2013103902@khu.ac.kr>
2018-03-03 15:43:42 +0900

    update

--- super_resolve.py
+++ super_resolve.py
@@ -4,13 +4,13 @@ import torch
 from torch.autograd import Variable
 from PIL import Image
 from torchvision.transforms import ToTensor
-
+from net.model10 import Net
 import numpy as np
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-parser.add_argument('--input_image', type=str, required=True, help='input image to use')
-parser.add_argument('--model', type=str, required=True, help='model file to use')
+parser.add_argument('--input_image', type=str,default='test.bmp' ,required=False, help='input image to use')
+parser.add_argument('--model_name', type=str,default='model' ,required=False, help='model file to use')
 parser.add_argument('--output_filename', type=str, help='where to save the output image')
 parser.add_argument('--cuda', action='store_true', required=True, help='use cuda')
 opt = parser.parse_args()
@@ -19,13 +19,12 @@ print(opt)
 img = Image.open(opt.input_image).convert('YCbCr')
 y, cb, cr = img.split()
 
-model = torch.load(opt.model)
+model =Net(2)
+model=load_state_dict(torch.load(opt.model))
 input = Variable(ToTensor()(y)).view(1, -1, y.size[1], y.size[0])
-
 if opt.cuda:
     model = model.cuda()
     input = input.cuda()
-
 out = model(input)
 out = out.cpu()
 out_img_y = out.data[0].numpy()
--- utils/dataset.py
+++ utils/dataset.py
@@ -19,7 +19,6 @@ class DatasetFromFolder(data.Dataset):
     def __init__(self, image_dir, input_transform=None, target_transform=None):
         super(DatasetFromFolder, self).__init__()
         self.image_filenames = [join(image_dir, x) for x in listdir(image_dir) if is_image_file(x)]
-
         self.input_transform = input_transform
         self.target_transform = target_transform
 
