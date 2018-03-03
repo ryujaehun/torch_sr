a438618074775d9d791ff0f65f54dec437d10984
ryujaehun <2013103902@khu.ac.kr>
2018-03-03 19:20:57 +0900

    update

--- super_resolve.py
+++ super_resolve.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 import argparse
+
 import torch
 from torch.autograd import Variable
 from PIL import Image
 from torchvision.transforms import ToTensor
 from net.model10 import Net
 import numpy as np
-
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
 parser.add_argument('--input_image', type=str,default='test.bmp' ,required=False, help='input image to use')
@@ -14,13 +14,13 @@ parser.add_argument('--model_name', type=str,default='model' ,required=False, he
 parser.add_argument('--output_filename', type=str, help='where to save the output image')
 parser.add_argument('--cuda', action='store_true', required=True, help='use cuda')
 opt = parser.parse_args()
-
 print(opt)
 img = Image.open(opt.input_image).convert('YCbCr')
 y, cb, cr = img.split()
+model=Net(2)
+model.load_state_dict(torch.load(opt.model_name))
+#model=torch.load("model_epoch_400.pth")
 
-model =Net(2)
-model=load_state_dict(torch.load(opt.model))
 input = Variable(ToTensor()(y)).view(1, -1, y.size[1], y.size[0])
 if opt.cuda:
     model = model.cuda()
@@ -31,7 +31,6 @@ out_img_y = out.data[0].numpy()
 out_img_y *= 255.0
 out_img_y = out_img_y.clip(0, 255)
 out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
-
 out_img_cb = cb.resize(out_img_y.size, Image.BICUBIC)
 out_img_cr = cr.resize(out_img_y.size, Image.BICUBIC)
 out_img = Image.merge('YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
