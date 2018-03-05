02ea4aa933d0e47069d6a5e9ab9838cdcd75371a
ryujaehun <2013103902@khu.ac.kr>
2018-03-05 10:54:22 +0900

    update psnr metric

--- super_resolve.py
+++ super_resolve.py
@@ -6,21 +6,33 @@ from PIL import Image
 from torchvision.transforms import ToTensor
 from net.model10 import Net
 import numpy as np
-
+import os
+import math
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-parser.add_argument('--input_image', type=str,default='test.bmp' ,required=False, help='input image to use')
-parser.add_argument('--model_name', type=str,default='model' ,required=False, help='model file to use')
-parser.add_argument('--output_filename', type=str, help='where to save the output image')
-parser.add_argument('--cuda', action='store_true', required=True, help='use cuda')
+parser.add_argument('--model_name', type=str,default='model_epoch_399.pth' ,required=False, help='model file to use')
+parser.add_argument('--output_filename', default='result',type=str, help='where to save the output image')
+parser.add_argument('--cuda', action='store_true', required=False, help='use cuda')
+parser.add_argument('--dataset', default='Set5', type=str,required=False, help='use cuda')
+def psnr(img1, img2):
+    mse = np.mean( (img1 - img2) ** 2 )
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 opt = parser.parse_args()
+model=torch.load(opt.model_name)
 
 print(opt)
-img = Image.open(opt.input_image).convert('YCbCr')
+curr=os.getcwd()
+os.chdir(os.path.join(curr,'dataset/data/'+opt.dataset+'/image_SRF_2'))
+for i in os.listdir():
+    if i[6]== '1':
+        if i[14:16]== "LR":
+            img = Image.open(i).convert('YCbCr')
+        else:
+            img_hr=Image.open(i)
 y, cb, cr = img.split()
-
-model =Net(2)
-model=load_state_dict(torch.load(opt.model))
 input = Variable(ToTensor()(y)).view(1, -1, y.size[1], y.size[0])
 if opt.cuda:
     model = model.cuda()
@@ -31,10 +43,8 @@ out_img_y = out.data[0].numpy()
 out_img_y *= 255.0
 out_img_y = out_img_y.clip(0, 255)
 out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
-
 out_img_cb = cb.resize(out_img_y.size, Image.BICUBIC)
 out_img_cr = cr.resize(out_img_y.size, Image.BICUBIC)
 out_img = Image.merge('YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
-
-out_img.save(opt.output_filename)
-print('output image saved to ', opt.output_filename)
+print('PSNR is ',psnr(np.array(out_img),np.array(img_hr)))
+os.chdir(curr)
